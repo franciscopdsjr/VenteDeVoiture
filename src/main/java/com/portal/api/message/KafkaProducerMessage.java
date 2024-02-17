4ca82fb2b6402c51887f2047e42e2c2a55d2601a
Francisco Pedroso Junior <franciscopj13@hotmail.com>
2024-02-17 14:01:41 -0300

    Feature/vtr 01 (#1)
    * First commit
    
    * DTO
    
    * Criando as classes Service e RestClient
    
    * Criando as classes Controller
    
    * Criando Kafka Producers

---
+++ src/main/java/com/portal/api/message/KafkaProducerMessage.java
@@ -0,0 +1,18 @@
+package VenteDeVoiture.src.main.java.com.portal.api.message;
+
+import VenteDeVoiture.src.main.java.com.portal.api.dto.CarPostDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducerMessage {
+    @Autowired
+    private KafkaTemplate<String, CarPostDTO> kafkaTemplate;
+
+    private final String KAFKA_TOPIC = "car-post-topic";
+
+    public void sendMessage(CarPostDTO carPostDTO){
+        kafkaTemplate.send(KAFKA_TOPIC, carPostDTO);
+    }
+}
